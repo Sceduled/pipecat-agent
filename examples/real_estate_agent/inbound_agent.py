73e550df9af4856fc6ebfa3ca38dd20f4ee34a47
kathiravan <codecurrent.in@gmail.com>
2026-06-19 10:19:35 +0530

    Fix deepgram model name

--- examples/real_estate_agent/inbound_agent.py
+++ examples/real_estate_agent/inbound_agent.py
@@ -85,7 +85,7 @@ async def run_inbound(
     stt = DeepgramSTTService(
         api_key=deepgram_api_key,
         settings=DeepgramSTTService.Settings(
-            model="nova-3-phonecall",
+            model="nova-2-phonecall",
             endpointing=150,
             utterance_end_ms=800,
             interim_results=True,
